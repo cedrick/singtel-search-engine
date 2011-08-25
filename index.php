fddc2f7769fccd51ff393babbab8f02d15c0c2cc
cedrick macatangay <cedrickmacatangay@gmail.com>
2011-08-25 21:30:37 +0800

    add index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <html>
 <head>
-<title>Singtel Search Portal</title>
+<title>CRM MEDIA3 SEARCH ENGINE</title>
 <link rel="shortcut icon" href="templates/images/favicon.ico" type="image/x-icon" />
 <link rel="stylesheet" href="library/ui-lightness/jquery-ui.css" />
 <script type="text/javascript" src="library/jquery.js"></script>
@@ -30,11 +30,10 @@ $(document).ready(function(){
 	</div>
 <?php include ("templates/style.css"); ?>
 <?php include ("templates/template.php"); ?>
-<div class="liner">
 <div class = "data" style = "display:none;">
 
 </div>
-  </div>
+
 	<style>
 		body{
 		font-family:"Lucida Grande", "Lucida Sans Unicode", Verdana, Arial, Helvetica, sans-serif;
@@ -47,6 +46,7 @@ $(document).ready(function(){
 		margin:0 auto;
 		width:400px;
 		padding:14px;
+		border-radius: 10px;
 		}
 		
 		/* ----------- stylized ----------- */
@@ -86,38 +86,49 @@ $(document).ready(function(){
 		float:left;
 		font-size:12px;
 		padding:4px 2px;
-		border:solid 1px #aacfe4;
+		border:solid 2px #0076C0;
 		width:200px;
 		margin:2px 0 20px 10px;
+		border-radius: 5px;
 		}
 		#stylized button{
 		clear:both;
 		margin-left:150px;
-		width:125px;
-		height:31px;
-		background:#666666 url(img/button.png) no-repeat;
+		width:92px;
+		height:28px;
+		background:#E6970C url(img/button.png) no-repeat;
 		text-align:center;
 		line-height:31px;
 		color:#FFFFFF;
 		font-size:11px;
 		font-weight:bold;
+		border-radius: 5px;
+		border:solid 3px  #000000;
+		}
+		
+		#stylized button:hover{
+		
+			background:#3497D4 url(img/button.png) no-repeat;
+		
 		}
 		.radioFields {
 			padding:4px 2px;
-			border:solid 1px #aacfe4;
+			border:solid 2px #0076C0;
 			width:195px;
 			margin:2px 0 20px 10px;
 			float: left;
 			background-color: #FFFFFF;
+			border-radius: 5px;
 		}
 		.inp_radio{
 			width: 20px;
 		}
+		
 	</style>
 	
 	<div id="stylized" class="myform">
 		<form id="form1" name="form1" method="post" action="">
-			  	<img src="templates/images/singtel.png" width="60" height="27" /><h1> Search Engine</h1>
+			 <h1> Media3 Search Engine</h1>
 			<p>You can enter Phone Number,Name, NRIC</p>
 			
 			<label>Search Box
@@ -128,10 +139,6 @@ $(document).ready(function(){
 			<label>Search By
 			</label>
 			<div class = "radioFields">
-				 <div>
-	            	<input name="rad_id" type="radio" id="radio6" class = "inp_radio" value="3"  <?php if($_REQUEST['rad_id'] == "3" || $_REQUEST['rad_id'] == NULL ) echo "checked";  ?>/>
-	               	<span>NRIC</span> 
-	            </div>
 	           <div>
 	            	<input name="rad_id" type="radio" id="radio5" class = "inp_radio" value="2"  <?php if($_REQUEST['rad_id'] == "2" || $_REQUEST['rad_id'] == NULL ) echo "checked";  ?>/>
 	               	<span>Name</span> 
